81106bf209ca474b43bda8ea93a15e41210832ce
kartikgopal01 <kartikgopal01@gmail.com>
2024-12-29 19:43:53 +0530

    Changes to be committed: 	modified:   .gitignore 	new file:   backend/README.md 	modified:   backend/app.js 	new file:   backend/controllers/captain.controller.js 	new file:   backend/controllers/map.controller.js 	new file:   backend/controllers/ride.controller.js 	modified:   backend/controllers/user.controller.js 	modified:   backend/db/db.js 	new file:   backend/middlewares/auth.middleware.js 	new file:   backend/models/blacklistToken.model.js 	new file:   backend/models/captain.model.js 	new file:   backend/models/ride.model.js 	modified:   backend/models/user.model.js 	modified:   backend/package-lock.json 	modified:   backend/package.json 	new file:   backend/routes/captain.routes.js 	new file:   backend/routes/maps.routes.js 	new file:   backend/routes/ride.routes.js 	modified:   backend/routes/user.routes.js 	modified:   backend/server.js 	new file:   backend/services/captain.service.js 	new file:   backend/services/maps.service.js 	new file:   backend/services/ride.service.js 	modified:   backend/services/user.service.js 	new file:   backend/socket.js 	new file:   frontend/README.md 	new file:   frontend/eslint.config.js 	new file:   frontend/index.html 	new file:   frontend/package-lock.json 	new file:   frontend/package.json 	new file:   frontend/postcss.config.js 	new file:   frontend/public/vite.svg 	new file:   frontend/src/App.css 	new file:   frontend/src/App.jsx 	new file:   frontend/src/assets/react.svg 	new file:   frontend/src/components/CaptainDetails.jsx 	new file:   frontend/src/components/ConfirmRide.jsx 	new file:   frontend/src/components/ConfirmRidePopUp.jsx 	new file:   frontend/src/components/FinishRide.jsx 	new file:   frontend/src/components/LiveTracking.jsx 	new file:   frontend/src/components/LocationSearchPanel.jsx 	new file:   frontend/src/components/LookingForDriver.jsx 	new file:   frontend/src/components/RidePopUp.jsx 	new file:   frontend/src/components/VehiclePanel.jsx 	new file:   frontend/src/components/WaitingForDriver.jsx 	new file:   frontend/src/context/CapatainContext.jsx 	new file:   frontend/src/context/SocketContext.jsx 	new file:   frontend/src/context/UserContext.jsx 	new file:   frontend/src/index.css 	new file:   frontend/src/main.jsx 	new file:   frontend/src/pages/CaptainHome.jsx 	new file:   frontend/src/pages/CaptainLogout.jsx 	new file:   frontend/src/pages/CaptainProtectWrapper.jsx 	new file:   frontend/src/pages/CaptainRiding.jsx 	new file:   frontend/src/pages/CaptainSignup.jsx 	new file:   frontend/src/pages/Captainlogin.jsx 	new file:   frontend/src/pages/Home.jsx 	new file:   frontend/src/pages/Riding.jsx 	new file:   frontend/src/pages/Start.jsx 	new file:   frontend/src/pages/UserLogin.jsx 	new file:   frontend/src/pages/UserLogout.jsx 	new file:   frontend/src/pages/UserProtectWrapper.jsx 	new file:   frontend/src/pages/UserSignup.jsx 	new file:   frontend/tailwind.config.js 	new file:   frontend/vite.config.js

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,49 +1,50 @@
-const mongoose=require('mongoose');
-const bcrypt=require('bcrypt');
-const jwt=require('jsonwebtoken');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-const userSchema =new mongoose.Schema({
-    fullname:{
-        firstname:{
-            type:String,
-            required:true,
-            minlength:[3,'first name must be at least 3 characters long']
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        firstname: {
+            type: String,
+            required: true,
+            minlength: [ 3, 'First name must be at least 3 characters long' ],
         },
-        lastname:{
-            type:String,
-            required:true,
-            minlength:[3,'last name must be at least 3 characters long']
+        lastname: {
+            type: String,
+            minlength: [ 3, 'Last name must be at least 3 characters long' ],
         }
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: [ 5, 'Email must be at least 5 characters long' ],
     },
-    password:{
-        type:String,
-        required:true,
-        select:false
+    password: {
+        type: String,
+        required: true,
+        select: false,
     },
-    socketId:{
-        type:String
-    }
+    socketId: {
+        type: String,
+    },
 })
 
-userSchema.methods.generateAuthToken=function(){
-    const token=jwt.sign({_id:this._id},process.env.JWT_SECRET_KEY);
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
     return token;
 }
 
-userSchema.methods.comparePassword=async function(password){
-   return await bcrypt.compare(password,this.password);
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
 }
 
-userSchema.methods.hashPassword=async function(password){
-    const salt=await bcrypt.genSalt(10);
-    return await bcrypt.hash(password,salt);
+userSchema.statics.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10);
 }
 
-const userModel=mongoose.model('user',userSchema);
+const userModel = mongoose.model('user', userSchema);
 
-module.explorts=userModel;
+
+module.exports = userModel;
